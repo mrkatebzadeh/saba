f1b2beac81856e32313486d8de7c81e828cd3d08
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2023-01-26 06:03:33 +0000

    Improve profiler cli

--- src/controller/profile.rs
+++ src/controller/profile.rs
@@ -32,32 +32,35 @@ impl PartialEq for BandwidthValuePercent {
 impl Eq for BandwidthValuePercent {}
 
 #[derive(Debug)]
-pub struct Profile {
-    name: String,
+pub struct ProfileRecord {
+    app: String,
     dataset_size: u16,
     number_of_nodes: u16,
-    weights: Vec<(BandwidthValuePercent, f32)>,
+    bw: BandwidthValuePercent,
+    time: u16,
 }
 
-impl Profile {
+impl ProfileRecord {
     #[allow(dead_code)]
     pub fn new(
-        name: String,
+        app: String,
         dataset_size: u16,
         number_of_nodes: u16,
-        weights: Vec<(BandwidthValuePercent, f32)>,
+        bw: BandwidthValuePercent,
+        time: u16,
     ) -> Self {
-        Profile {
-            name,
+        ProfileRecord {
+            app,
             dataset_size,
             number_of_nodes,
-            weights,
+            bw,
+            time,
         }
     }
 
     #[allow(dead_code)]
     pub fn name(&self) -> &String {
-        &self.name
+        &self.app
     }
 
     #[allow(dead_code)]
@@ -71,52 +74,14 @@ impl Profile {
     }
 
     #[allow(dead_code)]
-    pub fn weights(&self) -> &Vec<(BandwidthValuePercent, f32)> {
-        &self.weights
+    pub fn bw(&self) -> &BandwidthValuePercent {
+        &self.bw
     }
 
     #[allow(dead_code)]
-    pub fn get_weights_as_vector(&self) -> Vec<f32> {
-        let mut weights = Vec::new();
-        for (_, weight) in self.weights.iter() {
-            weights.push(*weight);
-        }
-        weights
+    pub fn time(&self) -> u16 {
+        self.time
     }
-
-    #[allow(dead_code)]
-    pub fn get_bandwidth_values(&self) -> Vec<u8> {
-        let mut bandwidth_values = Vec::new();
-        for (bandwidth_value, _) in self.weights.iter() {
-            bandwidth_values.push(bandwidth_value.value());
-        }
-        bandwidth_values
-    }
-
-    #[allow(dead_code)]
-    pub fn get_coefficients(&self) -> Vec<(u8, f32)> {
-        let mut coefficients = Vec::new();
-        for (bandwidth, weight) in self.weights.iter() {
-            coefficients.push((bandwidth.value(), *weight));
-        }
-        coefficients
-    }
-}
-
-#[allow(dead_code)]
-fn compare_vecs(
-    vec1: &Vec<(BandwidthValuePercent, f32)>,
-    vec2: &Vec<(BandwidthValuePercent, f32)>,
-) -> bool {
-    if vec1.len() != vec2.len() {
-        return false;
-    }
-    for i in 0..vec1.len() {
-        if vec1[i] != vec2[i] {
-            return false;
-        }
-    }
-    true
 }
 
 #[cfg(test)]
@@ -124,63 +89,29 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_profile() {
-        let profile = Profile::new(
-            "test".to_string(),
+    fn test_bandwidth_value_percent() {
+        assert_eq!(BandwidthValuePercent::Five.value(), 5);
+        assert_eq!(BandwidthValuePercent::Ten.value(), 10);
+        assert_eq!(BandwidthValuePercent::TwentyFive.value(), 25);
+        assert_eq!(BandwidthValuePercent::Fifty.value(), 50);
+        assert_eq!(BandwidthValuePercent::SeventyFive.value(), 75);
+        assert_eq!(BandwidthValuePercent::Ninety.value(), 90);
+        assert_eq!(BandwidthValuePercent::Hundred.value(), 100);
+    }
+
+    #[test]
+    fn test_profile_record() {
+        let profile_record = ProfileRecord::new(
+            "app".to_string(),
             100,
             10,
-            vec![
-                (BandwidthValuePercent::Five, 0.1),
-                (BandwidthValuePercent::Ten, 0.2),
-                (BandwidthValuePercent::TwentyFive, 0.3),
-                (BandwidthValuePercent::Fifty, 0.4),
-                (BandwidthValuePercent::SeventyFive, 0.5),
-                (BandwidthValuePercent::Ninety, 0.6),
-                (BandwidthValuePercent::Hundred, 1.0),
-            ],
+            BandwidthValuePercent::Fifty,
+            1000,
         );
-        assert_eq!(profile.name(), "test");
-        assert_eq!(profile.dataset_size(), 100);
-        assert_eq!(profile.number_of_nodes(), 10);
-        assert!(compare_vecs(
-            profile.weights(),
-            &vec![
-                (BandwidthValuePercent::Five, 0.1),
-                (BandwidthValuePercent::Ten, 0.2),
-                (BandwidthValuePercent::TwentyFive, 0.3),
-                (BandwidthValuePercent::Fifty, 0.4),
-                (BandwidthValuePercent::SeventyFive, 0.5),
-                (BandwidthValuePercent::Ninety, 0.6),
-                (BandwidthValuePercent::Hundred, 1.0),
-            ]
-        ));
-        assert_eq!(
-            profile.get_weights_as_vector(),
-            vec![0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 1.0]
-        );
-        assert_eq!(
-            profile.get_bandwidth_values(),
-            vec![
-                BandwidthValuePercent::Five.value(),
-                BandwidthValuePercent::Ten.value(),
-                BandwidthValuePercent::TwentyFive.value(),
-                BandwidthValuePercent::Fifty.value(),
-                BandwidthValuePercent::SeventyFive.value(),
-                BandwidthValuePercent::Ninety.value(),
-                BandwidthValuePercent::Hundred.value(),
-            ]
-        );
-        assert_eq!(
-            profile.get_coefficients(),
-            vec![
-                (5, 0.1),
-                (10, 0.2),
-                (25, 0.3),
-                (50, 0.4),
-                (75, 0.5),
-                (90, 0.6),
-                (100, 1.0),
-            ]
-        );
+        assert_eq!(profile_record.name(), &"app".to_string());
+        assert_eq!(profile_record.dataset_size(), 100);
+        assert_eq!(profile_record.number_of_nodes(), 10);
+        assert_eq!(profile_record.bw(), &BandwidthValuePercent::Fifty);
+        assert_eq!(profile_record.time(), 1000);
     }
 }
---
+++ src/controller/scheduler.rs
@@ -0,0 +1,219 @@
+use crate::connection::Connection;
+use crate::profile::ProfileRecord;
+use std::collections::HashMap;
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+pub enum AllocationAlgorithm {
+    InfiniBand,
+    IdealMaxMin,
+    SabaHierarchical,
+    SabaMean,
+}
+
+impl AllocationAlgorithm {
+    #[allow(dead_code)]
+    pub fn from_str(s: &str) -> Result<AllocationAlgorithm, String> {
+        match s {
+            "infiniband" => Ok(AllocationAlgorithm::InfiniBand),
+            "idealmaxmin" => Ok(AllocationAlgorithm::IdealMaxMin),
+            "sabahierarchical" => Ok(AllocationAlgorithm::SabaHierarchical),
+            "sabamean" => Ok(AllocationAlgorithm::SabaMean),
+            _ => Err(format!("Unknown allocation algorithm: {}", s)),
+        }
+    }
+
+    #[allow(dead_code)]
+    pub fn to_str(&self) -> &str {
+        match self {
+            AllocationAlgorithm::InfiniBand => "infiniband",
+            AllocationAlgorithm::IdealMaxMin => "idealmaxmin",
+            AllocationAlgorithm::SabaHierarchical => "sabahierarchical",
+            AllocationAlgorithm::SabaMean => "sabamean",
+        }
+    }
+}
+
+#[derive(Debug)]
+#[allow(dead_code)]
+pub struct AllocationRecord {
+    app: String,
+    src: String,
+    dst: String,
+    bw: f32,
+    priority: u8,
+}
+
+impl AllocationRecord {
+    #[allow(dead_code)]
+    fn new(app: String, src: String, dst: String, bw: f32, priority: u8) -> Self {
+        AllocationRecord {
+            app,
+            src,
+            dst,
+            bw,
+            priority,
+        }
+    }
+}
+
+#[derive(Debug)]
+#[allow(dead_code)]
+pub struct Scheduler {
+    allocation_algorithm: AllocationAlgorithm,
+    priority_levels: u8,
+    profile_table: Vec<ProfileRecord>,
+    allocation_table: Vec<AllocationRecord>,
+
+    slowdown_table: HashMap<String, Vec<f32>>,
+    sensitivity_table: HashMap<String, f32>,
+    priority_to_app_table: HashMap<u8, Vec<String>>,
+    connection_to_app_table: HashMap<Connection, String>,
+    app_to_priority_table: HashMap<String, u8>,
+}
+
+impl Scheduler {
+    #[allow(dead_code)]
+    pub fn new(
+        allocation_algorithm: AllocationAlgorithm,
+        priority_levels: u8,
+        profile_table: Vec<ProfileRecord>,
+    ) -> Self {
+        Scheduler {
+            allocation_algorithm,
+            priority_levels,
+            profile_table,
+            allocation_table: Vec::new(),
+            slowdown_table: HashMap::new(),
+            sensitivity_table: HashMap::new(),
+            priority_to_app_table: HashMap::new(),
+            connection_to_app_table: HashMap::new(),
+            app_to_priority_table: HashMap::new(),
+        }
+    }
+
+    #[allow(dead_code)]
+    pub fn allocate(&mut self) {
+        match self.allocation_algorithm {
+            AllocationAlgorithm::InfiniBand => self.allocate_infiniband(),
+            AllocationAlgorithm::IdealMaxMin => self.allocate_idealmaxmin(),
+            AllocationAlgorithm::SabaHierarchical => self.allocate_sabahierarchical(),
+            AllocationAlgorithm::SabaMean => self.allocate_sabamean(),
+        }
+    }
+
+    #[allow(dead_code)]
+    pub fn slowdown(&self, app: &str) -> Option<&Vec<f32>> {
+        self.slowdown_table.get(app)
+    }
+
+    #[allow(dead_code)]
+    pub fn sensitivity(&self, app: &str) -> Option<&f32> {
+        self.sensitivity_table.get(app)
+    }
+
+    #[allow(dead_code)]
+    pub fn priority_to_app(&self, priority: u8) -> Option<&Vec<String>> {
+        self.priority_to_app_table.get(&priority)
+    }
+
+    #[allow(dead_code)]
+    pub fn connection_to_app(&self, connection: &Connection) -> Option<&String> {
+        self.connection_to_app_table.get(connection)
+    }
+
+    #[allow(dead_code)]
+    pub fn app_to_priority(&self, app: &str) -> Option<&u8> {
+        self.app_to_priority_table.get(app)
+    }
+
+    #[allow(dead_code)]
+    pub fn allocation_table(&self) -> &Vec<AllocationRecord> {
+        &self.allocation_table
+    }
+
+    fn allocate_infiniband(&mut self) {
+        unimplemented!(); // TODO
+    }
+
+    fn allocate_idealmaxmin(&mut self) {
+        unimplemented!(); // TODO
+    }
+
+    fn allocate_sabahierarchical(&mut self) {
+        unimplemented!(); // TODO
+    }
+
+    fn allocate_sabamean(&mut self) {
+        unimplemented!(); // TODO
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_allocation_algorithm_from_str() {
+        assert_eq!(
+            AllocationAlgorithm::from_str("infiniband").unwrap(),
+            AllocationAlgorithm::InfiniBand
+        );
+        assert_eq!(
+            AllocationAlgorithm::from_str("idealmaxmin").unwrap(),
+            AllocationAlgorithm::IdealMaxMin
+        );
+        assert_eq!(
+            AllocationAlgorithm::from_str("sabahierarchical").unwrap(),
+            AllocationAlgorithm::SabaHierarchical
+        );
+        assert_eq!(
+            AllocationAlgorithm::from_str("sabamean").unwrap(),
+            AllocationAlgorithm::SabaMean
+        );
+        assert!(AllocationAlgorithm::from_str("unknown").is_err());
+    }
+
+    #[test]
+    fn test_allocation_algorithm_to_str() {
+        assert_eq!(AllocationAlgorithm::InfiniBand.to_str(), "infiniband");
+        assert_eq!(AllocationAlgorithm::IdealMaxMin.to_str(), "idealmaxmin");
+        assert_eq!(
+            AllocationAlgorithm::SabaHierarchical.to_str(),
+            "sabahierarchical"
+        );
+        assert_eq!(AllocationAlgorithm::SabaMean.to_str(), "sabamean");
+    }
+
+    #[test]
+    fn test_scheduler_new() {
+        let allocation_algorithm = AllocationAlgorithm::InfiniBand;
+        let priority_levels = 3;
+        let profile_table = vec![
+            ProfileRecord::new(
+                "app1".to_string(),
+                1,
+                2,
+                crate::profile::BandwidthValuePercent::Ten,
+                1,
+            ),
+            ProfileRecord::new(
+                "app1".to_string(),
+                1,
+                2,
+                crate::profile::BandwidthValuePercent::Ten,
+                2,
+            ),
+        ];
+        let scheduler =
+            Scheduler::new(allocation_algorithm.clone(), priority_levels, profile_table);
+        assert_eq!(scheduler.allocation_algorithm, allocation_algorithm);
+        assert_eq!(scheduler.priority_levels, priority_levels);
+        assert_eq!(scheduler.profile_table.len(), 2);
+        assert_eq!(scheduler.allocation_table.len(), 0);
+        assert_eq!(scheduler.slowdown_table.len(), 0);
+        assert_eq!(scheduler.sensitivity_table.len(), 0);
+        assert_eq!(scheduler.priority_to_app_table.len(), 0);
+        assert_eq!(scheduler.connection_to_app_table.len(), 0);
+        assert_eq!(scheduler.app_to_priority_table.len(), 0);
+    }
+}
